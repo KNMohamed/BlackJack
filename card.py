bf05710f917ae1a153bc89b58d06c54d702707a1
Khalid Mohamed <Khalid.N.Mohamed@outlook.com>
2023-04-10 13:49:59 +0000

    Initial commit

---
+++ card.py
@@ -0,0 +1,32 @@
+class Card:
+
+  def __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
+
+  def __str__(self):
+    return str(self.suit) + " of " + str(self.rank)
+
+  def __repr__(self):
+    return str(self)
+
+  def __eq__(self, other):
+    return self.suit == other.suit and self.rank == other.rank
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __hash__(self):
+    return hash(self.suit) + hash(self.rank)
+
+  def __lt__(self, other):
+    return self.rank < other.rank
+
+  def __le__(self, other):
+    return self.rank <= other.rank
+
+  def __gt__(self, other):
+    return self.rank > other.rank
+
+  def __ge__(self, other):
+    return self.rank >= other.rank
---
+++ deck.py
@@ -0,0 +1,27 @@
+from .card import Card
+from random import shuffle
+
+class Deck:
+  
+  def __init__(self):
+    all_suits = ['clubs', 'diamonds', 'hearts','spades']
+    all_ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    cards = [(suit,rank) for suit in all_suits for rank in all_ranks]
+    self.cards = [Card(suit, rank) for suit, rank in cards]
+    shuffle(self.cards)
+    
+  def shuffle(self):
+    shuffle(self.cards)
+
+  def deal(self):
+    return self.cards.pop()
+    
+  def deal_many(self, n):
+    return [self.cards.pop() for i in range(n)]
+
+  def __str__(self):
+    return " ".join([str(card) for card in self.cards])
+
+  def __repr__(self):
+    return str(self)
+    
