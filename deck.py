ad68ef0ed345dccad93f8478af22e9199f80ea6b
Khalid Mohamed <Khalid.N.Mohamed@outlook.com>
2023-04-10 13:52:21 +0000

    remove unused method

--- deck.py
+++ deck.py
@@ -1,21 +1,21 @@
 from .card import Card
 from random import shuffle
 
+
 class Deck:
-  
+
   def __init__(self):
-    all_suits = ['clubs', 'diamonds', 'hearts','spades']
-    all_ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    cards = [(suit,rank) for suit in all_suits for rank in all_ranks]
+    all_suits = ['clubs', 'diamonds', 'hearts', 'spades']
+    all_ranks = [
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'
+    ]
+    cards = [(suit, rank) for suit in all_suits for rank in all_ranks]
     self.cards = [Card(suit, rank) for suit, rank in cards]
-    shuffle(self.cards)
-    
-  def shuffle(self):
     shuffle(self.cards)
 
   def deal(self):
     return self.cards.pop()
-    
+
   def deal_many(self, n):
     return [self.cards.pop() for i in range(n)]
 
@@ -24,4 +24,3 @@ class Deck:
 
   def __repr__(self):
     return str(self)
-    
